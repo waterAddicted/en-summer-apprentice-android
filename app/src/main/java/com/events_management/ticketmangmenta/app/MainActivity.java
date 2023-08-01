788b1cd827bd91fa0b0b709642a7f36e75654c20
Bogdan B <bodobogdan57@gmail.com>
2023-08-01 13:11:29 +0300

    ?

---
+++ app/src/main/java/com/events_management/ticketmangmenta/app/MainActivity.java
@@ -0,0 +1,14 @@
+package com.events_management.ticketmangmenta.app;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInsanceState){
+        super.onCreate(savedInsanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
